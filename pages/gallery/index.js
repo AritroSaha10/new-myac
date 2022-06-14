c762fea59438176769efca6edefcc6c257928d42
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-06-14 12:48:28 -0400

    fix: use specified thumbnail for gallery

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -8,11 +8,13 @@ import { FiLink2 } from "react-icons/fi";
 import Layout from "../../components/Layout";
 import airtableDB from "../../db/airtable";
 
+import ConnectImage from "../../imgs/connect.jpg"
+
 export async function getStaticProps() {
     let galleryInfo = [];
 
     const records = await airtableDB("Gallery").select({
-        fields: ["Name", "Date", "GalleryLink", "Photos"],
+        fields: ["Name", "Date", "GalleryLink", "Thumbnail"],
         sort: [{ field: "Date", direction: "desc" }],
     }).all();
 
@@ -21,17 +23,11 @@ export async function getStaticProps() {
             name: fields.Name,
             date: fields.Date,
             route: fields.GalleryLink,
-            images: fields.Photos.map(image => ({
-                src: image.url,
-                width: image.width,
-                height: image.height,
-                blurDataURL: image.thumbnails.small.url
-            })),
             thumbnail: {
-                src: fields.Photos[0].url,
-                blurDataURL: fields.Photos[0].thumbnails.small.url,
-                width: fields.Photos[0].width,
-                height: fields.Photos[0].height
+                src: fields.Thumbnail[0].url,
+                blurDataURL: fields.Thumbnail[0].thumbnails.small.url,
+                width: fields.Thumbnail[0].width,
+                height: fields.Thumbnail[0].height
             }
         })
     })
@@ -46,7 +42,29 @@ export async function getStaticProps() {
 export default function GalleryIndex({ galleryInfo }) {
     return (
         <Layout name="Gallery">
-            <div className="p-16 md:py-24">
+            <header className="h-[30vh] lg:h-[44vh] relative">
+                {/* Background image using Next.js Image, taken from here: https://github.com/vercel/next.js/discussions/18357#discussioncomment-132523 */}
+                <Image
+                    src={ConnectImage}
+                    placeholder="blur"
+                    objectFit="cover"
+                    objectPosition="center top"
+                    alt="Hero Image"
+                    layout="fill"
+                    className="saturate-150"
+                    quality={100}
+                />
+            </header>
+
+            <div className="flex p-16 flex-col items-center items-left">
+                <div className="flex flex-col text-center">
+                    <h3 className="text-lg text-blue-500">Memories from our events</h3>
+                    <h1 className="text-5xl lg:text-5xl text-black font-bold">Gallery</h1>
+                </div>
+            </div>
+
+            <div className="p-8 pt-0 md:pb-16">
+                
                 {galleryInfo.length === 0 ? (
                     <div className="flex flex-col items-center w-full">
                         <h1 className="text-5xl text-gray-600 font-medium text-center mb-4">
@@ -61,11 +79,12 @@ export default function GalleryIndex({ galleryInfo }) {
                     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 lg:px-16 xl:px-32">
                         {galleryInfo.map(album => (
                             <div key={album.name}> {/* Wrapper div to allow the cards to resize themselves */}
-                                <div className="flex flex-col p-4 bg-white outline outline-gray-200 drop-shadow-lg rounded-lg gap-2">
+                                <div className="flex flex-col p-4 gap-2">
                                     <Image
                                         src={album.thumbnail}
                                         width={400}
                                         height={400}
+                                        quality={80}
                                         placeholder="blur"
                                         objectFit="cover"
                                         objectPosition="center"
