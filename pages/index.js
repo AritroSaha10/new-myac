34fff6c8bab487723edbe970d2718eeb3ffc6f18
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-06-14 19:47:07 -0400

    fix: changed index hero image location

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
-import HeroImage from '../imgs/everyone.png'
+import HeroImage from '../public/everyone.png'
 import SummaryImage from '../imgs/summary.jpg'
 import ConnectImage from '../imgs/connect.jpg'
 import Logo from '../imgs/logo.png'
