1b669a5faa878123b4237b8f669da322f5f58d24
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2024-05-07 14:39:59 -0400

    add loading bar between page transitions

---
+++ components/LoadingBar.js
@@ -0,0 +1,17 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+    minimum: 0.3,
+    easing: 'ease',
+    speed: 500,
+    showSpinner: true,
+});
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+const LoadingBar = () => null;
+export default LoadingBar;
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { AnimatePresence } from 'framer-motion'
 
+import dynamic from 'next/dynamic';
+const ProgressBar = dynamic(() => import('../components/LoadingBar'), { ssr: false });
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -14,6 +17,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <AnimatePresence exitBeforeEnter>
       <Component key={router.pathname} {...pageProps} />
+      <ProgressBar />
     </AnimatePresence>
   )
 }
